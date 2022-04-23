f1e42399c4dafe443d38644a2aa080e6185b0e8d
Adidesing <104267322+Adidesing@users.noreply.github.com>
2022-04-23 16:42:49 +0100

    Add files via upload

---
+++ zadacha/js/hamburgermenu.js
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+</script> 
